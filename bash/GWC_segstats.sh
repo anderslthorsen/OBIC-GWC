f1a8bb87f8e69087ead51ea98140add897a8d15d
Anders Lillevik Thorsen <anders.l.thorsen@gmail.com>
2023-11-20 20:52:25 +0100

    Update GWC_segstats.sh

--- bash/GWC_segstats.sh
+++ bash/GWC_segstats.sh
@@ -18,54 +18,50 @@ echo 'SUBJECTS_DIR is '$SUBJECTS_DIR
 #subject=sub-01subject001
 
 # Loop over subjects using a line seperated list of subject IDs
-#for subject in `cat GWC/GWC_include20Dec2021.txt`; do
+for subject in `cat GWC/GWC_include20Dec2021.txt`; do
 
 	# Loop over hemispheres
-	#for hemi in lh rh; do
+	for hemi in lh rh; do
 
 		# Run mri_segstats for cortical thickness (using subject aparc)
-		#mri_segstats --annot ${subject} ${hemi} aparc --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.thickness --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.thickness.DK
+		mri_segstats --annot ${subject} ${hemi} aparc --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.thickness --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.thickness.DK
 	
 		# Run mri_segstats for subcortical volume (using subject aseg)
-		#mri_segstats --annot ${subject} ${hemi} aseg --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.thickness --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.thickness.DK
+		mri_segstats --annot ${subject} ${hemi} aseg --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.thickness --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.thickness.DK
 
 		# Run mri_segstats for GWC (using subject aparc)
-		#mri_segstats --annot $subject ${hemi} aparc --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.nu.w-g.avg.mgh --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.GWC.DK
+		mri_segstats --annot $subject ${hemi} aparc --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.nu.w-g.avg.mgh --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.GWC.DK
 		
 		# Run mri_segstats for WM intensity (using subject aparc)
-		#mri_segstats --annot $subject ${hemi} aparc --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.nu.avg.wm.mgh --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.WM.DK
+		mri_segstats --annot $subject ${hemi} aparc --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.nu.avg.wm.mgh --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.WM.DK
 		
 		# Run mri_segstats for GM intensity (using subject aparc)
-		#mri_segstats --annot $subject ${hemi} aparc --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.nu.avg.gm.mgh --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.GM.DK
-
-		# Run mri_segstats for GWC (using fsaverage aparc). This is currently not used as the number of vertices would be different across subjects and between thickness vs GWC due to the transfomration to fsavarage space.
-		#mri_segstats --annot fsaverage ${hemi} aparc --i $SUBJECTS_DIR/$subject/surf/lh.nu.w-g.avg_fsaverage.10.mgh --sum $output_dir/test.gwc.lh.sum
-
-	#done
-#done
+		mri_segstats --annot $subject ${hemi} aparc --i ${SUBJECTS_DIR}/${subject}/surf/${hemi}.nu.avg.gm.mgh --sum ${SUBJECTS_DIR}/${subject}/stats/${hemi}.GM.DK
+	done
+done
 
 cd ${output_dir}
 
 # Run aseg2stats to get a table of the results
 
 # Cortical thickness
-#asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=lh.thickness.DK -m mean --tablefile N848_thickness.lh.DK.csv -v
-#asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=rh.thickness.DK -m mean --tablefile N848_thickness.rh.DK.csv -v
+asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=lh.thickness.DK -m mean --tablefile N848_thickness.lh.DK.csv -v
+asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=rh.thickness.DK -m mean --tablefile N848_thickness.rh.DK.csv -v
 
 # Subcortical volume
 asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --tablefile N848_subcortical_volume.bilateral.DK.csv -v
 
 # GWC
-#asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=lh.GWC.DK -m mean --tablefile N848_GWC.lh.DK.csv -v
-#asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=rh.GWC.DK -m mean --tablefile N848_GWC.rh.DK.csv -v
+asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=lh.GWC.DK -m mean --tablefile N848_GWC.lh.DK.csv -v
+asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=rh.GWC.DK -m mean --tablefile N848_GWC.rh.DK.csv -v
 
 # GM signal intensity
-#asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=lh.GM.DK -m mean --tablefile N848_GM.lh.DK.csv -v
-#asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=rh.GM.DK -m mean --tablefile N848_GM.rh.DK.csv -v
+asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=lh.GM.DK -m mean --tablefile N848_GM.lh.DK.csv -v
+asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=rh.GM.DK -m mean --tablefile N848_GM.rh.DK.csv -v
 
 # WM signal intensity
-#asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=lh.WM.DK -m mean --tablefile N848_WM.lh.DK.csv -v
-#asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=rh.WM.DK -m mean --tablefile N848_WM.rh.DK.csv -v
+asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=lh.WM.DK -m mean --tablefile N848_WM.lh.DK.csv -v
+asegstats2table --subjectsfile=/data/OBIC/Freesurfer/GWC/GWC_include20Dec2021.txt --stats=rh.WM.DK -m mean --tablefile N848_WM.rh.DK.csv -v
 
 
 
