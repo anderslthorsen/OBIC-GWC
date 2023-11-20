9ff4988d0b09b57d9dd14a2e44f1c6ee7ee3b752
Anders Lillevik Thorsen <anders.l.thorsen@gmail.com>
2023-11-20 21:03:52 +0100

    Update freeview_standard.sh

--- bash/freeview_standard.sh
+++ bash/freeview_standard.sh
@@ -2,10 +2,6 @@
 # Written by Anders Lillevik Thorsen, September 2021
 # This script takes the input of a subject ID and loads the relevant T1, brainmask, segmentation, pial and white matter surfaces from recon-all in Freesurfer
 
-
-#outputdir=/data/OBIC/Freesurfer # Where you want the Freesurfer data to end up
-#cd ${outputdir}
-
 freeview -v $1/mri/T1.mgz \
 $1/mri/wm.mgz \
 $1/mri/brainmask.mgz \
